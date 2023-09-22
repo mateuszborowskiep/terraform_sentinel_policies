f9c18ec4858a60eb9e69beeb8a50b5f6fe008d8c
mateuszborowskiep <borovskimateusz@gmail.com>
2023-09-22 05:48:32 +0000

    changes in region and zone

--- main.tf
+++ main.tf
@@ -19,13 +19,13 @@ variable "image" {
 
 provider "google" {
     project = "${var.gcp_project}"
-    region = "us-east1"
+    region = "europe-west1"
 }
 
 resource "google_compute_instance" "demo" {
     name = "${var.instance_name}"
     machine_type = "${var.machine_type}"
-    zone = "us-east1-b"
+    zone = "europe-west1-b"
 
     boot_disk {
         initialize_params {
