f3e47b72ee314645970d3c324012ac369c941443
mateuszborowskiep <borovskimateusz@gmail.com>
2023-09-22 06:13:01 +0000

    changes in variables

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-variable "gcp_project" {
-    description = "GCP project name"
-}
-
-variable "machine_type" {
-    description = "GCP machine type"
-    default = "n1-standard-1"
-}
-
-variable "instance_name" {
-    description = "GCP instance name"
-    default = "demo"
-}
-
-variable "image" {
-    description = "GCP image"
-    default = "debian-cloud/debian-9"
-}
-
 provider "google" {
     project = "${var.gcp_project}"
     region = "europe-west1"
